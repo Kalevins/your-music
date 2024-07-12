023bbe4671f24998c5ea137204d0b831af9e3086
Kevin Muñoz Rengifo <kevinmunoz01@hotmail.com>
2024-07-12 10:19:02 -0500

    [Add]  Info login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,17 +1,73 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useForm } from "react-hook-form"
 import { ErrorMessage } from "@hookform/error-message"
+import { motion } from 'framer-motion';
+import { BsInfoLg } from "react-icons/bs";
 
 import { authContext } from '@/contexts';
 import { LoadingScreen } from '@/components';
 
 import styles from './styles.module.scss';
 
+const variantsContainer = {
+  open: {
+    width: 'auto',
+    transition: {
+      stiffness: 400,
+      restDelta: 2
+    }
+  },
+  closed: {
+    width: '0px',
+    transition: {
+      stiffness: 400,
+      damping: 40
+    }
+  }
+};
+
+const variantsInfo = {
+  open: {
+    transition: {
+      staggerChildren: 0.07,
+      delayChildren: 0.2
+    }
+  },
+  closed: {
+    transition: {
+      staggerChildren: 0.05,
+      staggerDirection: -1
+    }
+  }
+};
+
+const variantsText = {
+  open: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      y: {
+        stiffness: 1000
+      }
+    }
+  },
+  closed: {
+    x: 50,
+    opacity: 0,
+    transition: {
+      y: {
+        stiffness: 1000
+      }
+    }
+  }
+};
+
 export function Login() {
   const { login } = useContext(authContext)
   const navigate = useNavigate()
   const [loadingLogin, setLoadingLogin] = useState(false)
+  const [isOpen, setIsOpen] = useState(true)
 
   const {
     register,
@@ -20,6 +76,12 @@ export function Login() {
   } = useForm({
     criteriaMode: 'all',
   })
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsOpen(false)
+    }, 5000)
+  }, [])
 
   const onSubmit = async ({ username, password }) => {
     setLoadingLogin(true)
@@ -99,6 +161,31 @@ export function Login() {
             </button>
           </form>
         </div>
+        <button className={styles.primary} id={styles.infoButton} onClick={() => {setIsOpen(!isOpen)}}>
+          <BsInfoLg />
+        </button>
+        <motion.div
+          initial={true}
+          animate={isOpen ? "open" : "closed"}
+          variants={variantsContainer}
+          className={styles.infoContainer}
+        >
+          <motion.div
+            className={styles.info}
+            variants={variantsInfo}
+          >
+              <motion.p
+                variants={variantsText}
+              >
+                To enter you do not need to register, just enter
+              </motion.p>
+              <motion.p
+                variants={variantsText}
+              >
+                 the email and password that meet the conditions
+              </motion.p>
+          </motion.div>
+        </motion.div>
       </main>
     </>
   )
